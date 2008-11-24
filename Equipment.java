81c856a3fa57488f2ab6055425b59c290e7c6378
im.a.Hypocrite <im.a.Hypocrite@6f14d1e4-ba63-11dd-b612-3927438af0ba>
2008-11-24 23:05:20 +0000

    added most equipment classes. equip/unequip now change hero's stats by equipments modifiers

--- Equipment.java
+++ Equipment.java
@@ -1,22 +1,71 @@
 
 public abstract class Equipment extends Item
 {
-    public final int HEAD = 0;
-    public final int SHOULDER = 1;
-    public final int HANDS = 2;
-    public final int CHEST = 3;
-    public final int GREAVES = 4;
-    public final int BOOTS = 5;
-    public final int CLOAK = 6;
-    public final int BELT = 7;
-    public final int TRINKET = 8;
-    public final int NECKLACE = 9;
-    public final int WEAPON = 10;
-    public final int OFFHAND = 11;
-    public final int TOME = 12;
+    public final static int HEAD = 0;
+    public final static int SHOULDER = 1;
+    public final static int HANDS = 2;
+    public final static int CHEST = 3;
+    public final static int GREAVES = 4;
+    public final static int BOOTS = 5;
+    public final static int CLOAK = 6;
+    public final static int BELT = 7;
+    public final static int TRINKET = 8;
+    public final static int NECKLACE = 9;
+    public final static int WEAPON = 10;
+    public final static int OFFHAND = 11;
+    public final static int TOME = 12;
     
+    private int strength, dexterity, toughness, intelligence, wisdom, defense;
     private int levelRequirement;
     
+    public int getStrengthMod()
+    {
+        return strength;
+    }
+    public void setStrengthMod(int str)
+    {
+        strength = str;
+    }
+    public int getDexterityMod()
+    {
+        return dexterity;
+    }
+    public void setDexterityMod(int dex)
+    {
+        dexterity = dex;
+    }
+    public int getToughnessMod()
+    {
+        return toughness;
+    }
+    public void setToughnessMod(int tough)
+    {
+        toughness = tough;
+    }
+    public int getIntelligenceMod()
+    {
+        return intelligence;
+    }
+    public void setIntelligenceMod(int intel)
+    {
+        intelligence = intel;
+    }
+    public int getWisdomMod()
+    {
+        return wisdom;
+    }
+    public void setWisdomMod(int wis)
+    {
+        wisdom = wis;
+    }
+    public int getDefenseMod()
+    {
+        return defense;
+    }
+    public void setDefenseMod(int def)
+    {
+        defense = def;
+    }
     public int getLevelRequirement()
     {
         return levelRequirement;

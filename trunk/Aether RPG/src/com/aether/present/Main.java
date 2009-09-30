3f2cd06debe9072b3ab80491c6c6c7ddadd46ac9
Captain.queezy@gmail.com <Captain.queezy@gmail.com@6f14d1e4-ba63-11dd-b612-3927438af0ba>
2009-09-30 08:24:03 +0000

    Hopefully made it a single download all and run application sequence.  All the libraries are contained and no longer are we needing to download external source files in order for us to develop our application.
    Note: GBui.jar has a slight modification from the [HEAD], modified 2 files PolledRootNode and BRootNode.  These implemented a final method findCollisions, and hasCollisions.  This version removed those methods in the source.

--- trunk/Aether RPG/src/com/aether/present/Main.java
+++ trunk/Aether RPG/src/com/aether/present/Main.java
@@ -2,9 +2,20 @@ package com.aether.present;
 
 import org.gap.jseed.ServiceStore;
 
+import com.aether.present.css.BssColor;
+import com.aether.present.css.BssFontStyle;
+import com.aether.present.css.BssStyleClass;
+import com.aether.present.css.BssWriter;
 import com.aether.present.state.ShutdownService;
 import com.jme.app.AbstractGame;
+import com.jme.input.InputHandler;
 import com.jme.input.MouseInput;
+import com.jme.util.Timer;
+import com.jmex.bui.BStyleSheet;
+import com.jmex.bui.BuiSystem;
+import com.jmex.bui.PolledRootNode;
+import com.jmex.bui.bss.BStyleSheetUtil;
+import com.jmex.bui.provider.DefaultResourceProvider;
 import com.jmex.game.StandardGame;
 import com.jmex.game.state.GameStateManager;
 
@@ -19,7 +30,30 @@ public class Main {
 		store.bind(ShutdownService.class, ShutdownService.class);
 		store.bind(MainWindow.class, MainWindow.class);
 
+		loadBStyleSheets();
+		
 		bindGameState(store);
+	}
+
+	private static void loadBStyleSheets() {
+		try {
+			BssWriter writer = new BssWriter();
+			
+			BssStyleClass button = writer.button();
+			button.setFont("Helvetica", BssFontStyle.BOLD, 24);
+			button.setColor(BssColor.RED);
+			button.setBackground(BssColor.BLUE);
+			
+			BssStyleClass label = writer.label();
+			label.setFont("Helvetica", BssFontStyle.ITALIC, 30);
+			label.setColor(BssColor.GREEN);
+			label.clearBackground();
+			
+			BStyleSheet styleSheet = BStyleSheetUtil.getStyleSheet(writer.asReader());
+			BuiSystem.init(new PolledRootNode(Timer.getTimer(), new InputHandler()), styleSheet);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	private static void bindGameState(ServiceStore store) {
---
+++ trunk/Aether RPG/src/com/aether/present/css/BssStyleClass.java
@@ -0,0 +1,129 @@
+package com.aether.present.css;
+
+import org.gap.jseed.contract.Contract;
+
+import com.jmex.bui.property.FontProperty;
+
+public class BssStyleClass {
+	private static final int TOP = 0;
+	private static final int RIGHT = 1;
+	private static final int BOTTOM = 2;
+	private static final int LEFT = 3;
+
+	private final BssWriter cssWriter;
+
+	private Integer[] padding = {3, 5, null, null};
+	private BssColor color = BssColor.RED;
+	private BssColor backgroundColor = BssColor.BLUE;
+	private String backgroundStyle = "solid";
+	private BssTextAlign textAlign = BssTextAlign.CENTER;
+	private BssVerticalAlign verticalAlign = BssVerticalAlign.CENTER;
+	private BssTextEffect textEffect = BssTextEffect.NONE;
+	private FontProperty font = new FontProperty();
+	private final String name;
+	
+	/*package*/ BssStyleClass(String name, BssWriter cssWriter) {
+		this.name = name;
+		this.cssWriter = cssWriter;
+		font.family = "Dialog";
+		font.style = BssFontStyle.BOLD.toString();
+		font.size = 12;
+	}
+
+	public BssWriter end() {
+		return cssWriter;
+	}
+
+	public void setColor(BssColor newColor) {
+		color = newColor;
+	}
+
+	public void setBackground(BssColor newBackground) {
+		this.backgroundStyle = "solid";
+		this.backgroundColor  = newBackground;
+	}
+
+	public void setTextAlign(BssTextAlign newTextAlign) {
+		textAlign = newTextAlign;
+	}
+	
+	public void setVerticalAlign(BssVerticalAlign newVerticalAlign) {
+		verticalAlign = newVerticalAlign;
+	}
+
+	public void setTextEffect(BssTextEffect newTextEffect) {
+		textEffect = newTextEffect;
+	}
+	
+	public void setFont(String family, BssFontStyle style, int size) {
+		Contract.argumentNotNull(family, "Family font must be specified");
+		Contract.argumentNotNull(style, "Style font must be specified");
+		font.family = family;
+		font.style = style.toString();
+		font.size = size;
+	}
+	
+	public String writeBss() {
+		return name + " {\n" + 
+				writeFont() + 
+				writePaddingAction() +
+			   	writeAction("color", color) +
+			   	writeBackground() + 
+			   	writeAction("text-align", textAlign) + 
+			   	writeAction("vertical-align", verticalAlign) +
+			   	writeAction("text-effect", textEffect) +
+			   "}";
+	}
+
+
+	private String writeFont() {
+		return String.format("\tfont: %s %s %s;\n", font.family, font.style, font.size);
+	}
+
+	private String writePaddingAction() {
+		StringBuffer buffer = new StringBuffer("\tpadding:");
+		
+		for (Integer each : padding) {
+			if (each != null) {
+				buffer.append(" " + each);
+			} else {
+				break;
+			}
+		}
+		
+		return buffer + ";\n";
+	}
+
+	private String writeAction(String name, Object value) {
+		if (value == null) {
+			return "";
+		}
+		return String.format("\t%s: %s;\n", name, value);
+	}
+
+	private String writeBackground() {
+		return String.format("\tbackground: %s %s;\n", backgroundStyle, backgroundColor);
+	}
+
+	public void setPadding(int allSides) {
+		padding[TOP] = allSides;
+		padding[RIGHT] = padding[BOTTOM] = padding[LEFT] = null;
+	}
+
+	public void setPadding(int topAndBottom, int leftAndRight) {
+		padding[TOP] = topAndBottom;
+		padding[RIGHT] = leftAndRight;
+		padding[BOTTOM] = padding[LEFT] = null;
+	}
+	
+	public void setPadding(int top, int right, int bottom, int left) {
+		padding[TOP] = top;
+		padding[RIGHT] = right;
+		padding[BOTTOM] = bottom;
+		padding[LEFT] = left;
+	}
+
+	public void clearBackground() {
+		backgroundStyle = "blank";
+	}
+}

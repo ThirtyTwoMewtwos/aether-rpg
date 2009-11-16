14e415f75f7d43d126ca574d2590286bf907295d
Captain.queezy <Captain.queezy@gmail.com>
2009-11-16 16:46:37 +0000

    - Fixed Persona to have P as the key to show and hide the window. - Added the missing TestPersonaWindow

---
+++ Aether RPG/test/com/aether/present/hud/TestPersonaWindow.java
@@ -0,0 +1,70 @@
+package com.aether.present.hud;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.AWTException;
+import java.awt.event.KeyEvent;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.aether.gbui.BKeyboard;
+import com.aether.gbui.NameOperatorSearch;
+import com.aether.gbui.operators.BComponentOperatorUtil;
+import com.aether.gbui.operators.BLabelOperator;
+import com.aether.present.Main;
+import com.aether.present.state.CreateCharacterPage;
+import com.aether.present.state.MainMenuPage;
+import com.jmex.bui.BWindow;
+
+public class TestPersonaWindow {
+	
+	private BWindow personaWindow;
+
+	@Before
+	public void setUp() throws Exception {
+		Main.startGame();
+		CreateCharacterPage newCampain = new MainMenuPage().clickNewCampain();
+		newCampain.loadDummyData();
+		newCampain.clickFinish();
+		
+		personaWindow = BComponentOperatorUtil.windowWithId(PersonaView.PERSONA_ID);
+	}
+	
+	@Test
+	public void test_Window_is_available() throws Exception {
+		togglePersonaView();
+		assertTrue(personaWindow.isVisible());
+		togglePersonaView();
+		assertFalse(personaWindow.isVisible());
+	}
+
+	private void togglePersonaView() throws AWTException {
+		new BKeyboard().typeKey(KeyEvent.VK_P);
+	}
+	
+	@Test
+	public void test_Statistics_have_been_set_from_character_sheet() throws Exception {
+		togglePersonaView();
+		BLabelOperator strength = getStatisticLabel(PersonaView.STRENGTH_STATISTIC);
+		assertEquals("10", strength.getText());
+		BLabelOperator dexterity = getStatisticLabel(PersonaView.DEXTERITY_STATISTIC);
+		assertEquals("10", dexterity.getText());
+		BLabelOperator intelligence = getStatisticLabel(PersonaView.INTELLIGENCE_STATISTIC);
+		assertEquals("10", intelligence.getText());
+		BLabelOperator wisdom = getStatisticLabel(PersonaView.WISDOM_STATISTIC);
+		assertEquals("10", wisdom.getText());
+	}
+
+	private BLabelOperator getStatisticLabel(String stat) {
+		return new BLabelOperator(personaWindow, new NameOperatorSearch(stat));
+	}
+
+	@After
+	public void tearDown() {
+		Main.shutdown();
+	}
+}

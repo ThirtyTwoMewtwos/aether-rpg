a38710c16ae4b1e98b4cb7fb32ecb3ea882b9290
im.a.hypocrite <im.a.hypocrite@6f14d1e4-ba63-11dd-b612-3927438af0ba>
2009-11-25 16:27:21 +0000

    Updated Persona window, it now holds all information, except bio.   Updated nbproject information and build file related to netbeans so that users who download aether for netbeans, there is no effort required to run. Small changes to CharcterSheet.

--- trunk/Aether RPG/src/com/aether/present/hud/PersonaView.java
+++ trunk/Aether RPG/src/com/aether/present/hud/PersonaView.java
@@ -2,16 +2,54 @@ package com.aether.present.hud;
 
 public interface PersonaView {
 	public static final String PERSONA_ID = "hud.persona.view";
+    
+    public static final String LEVEL_STATISTIC = "LEVEL";
+    public static final String EXP_STATISTIC = "EXP";
+    public static final String NEXTLEVEL_STATISTIC = "NEXT";
+    public static final String HP_STATISTIC = "HP";
+    public static final String MP_STATISTIC = "MP";
+
 	public static final String STRENGTH_STATISTIC = "STR";
 	public static final String DEXTERITY_STATISTIC = "DEX";
 	public static final String INTELLIGENCE_STATISTIC = "INT";
+    public static final String TOUGHNESS_STATISTIC = "CON";
 	public static final String WISDOM_STATISTIC = "WIS";
+
+    public static final String DEFENSE_STATISTIC = "DEF";
+    public static final String MELEE_STATISTIC = "MELEE";
+    public static final String RANGE_STATISTIC = "RANGE";
+    public static final String MAGIC_STATISTIC = "MAGIC";
+    public static final String BLOCK_STATISTIC = "BLOCK";
+    public static final String DISPELL_STATISTIC ="DSPELL";
+    public static final String CRITICAL_STATISTIC = "CRIT";
+    public static final String DODGE_STATISTIC = "DODGE";
+
 
 	void setPresenter(PersonaPresenter anyObject);
 
 	void activate();
 	
 	void deactivate();
+
+    void setName(String n);
+
+    void setRace(String r);
+
+    void setClassification(String c);
+
+    void setSex(String s);
+    
+    void setLevel(int i);
+
+    void setExp(int i);
+
+    void setNextLevel(int i);
+
+    void setHP(int i);
+
+    void setMP(int i);
+
+    void setToughness(int i);
 
 	void setStrength(int i);
 
@@ -22,4 +60,20 @@ public interface PersonaView {
 	void setWisdom(int i);
 
 	void setVisible(boolean b);
+
+    void setDefense(int i);
+
+    void setMelee(double d);
+
+    void setRange(double d);
+
+    void setMagic(double d);
+
+    void setBlock(double d);
+
+    void setCrit(double d);
+
+    void setDodge(double d);
+
+    void setDispell(double d);
 }

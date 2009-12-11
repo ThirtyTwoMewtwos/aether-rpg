baea2f5c38a883a3bc534b27585516800c6bcc31
im.a.Hypocrite <im.a.Hypocrite@6f14d1e4-ba63-11dd-b612-3927438af0ba>
2009-12-11 06:59:39 +0000

    -Created Login Window which will take place of "MainWindow" which in turn becomes an intermediate screen. -The login window will be the first screen the user sees (after splash screens) -Once the login is successful, the server loads any characters, or if none, only offers to create a character. -LoginWindow has *not* been viewed yet and is not finished.

---
+++ trunk/Aether RPG/src/com/aether/present/LoginWindow.java
@@ -0,0 +1,165 @@
+package com.aether.present;
+
+/*
+ * LoginWindow.java
+ *
+ * Copyright (c) 2008, Tyler Hoersch
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Wisconsin Oshkosh nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+import org.gap.jseed.injection.annotation.Singleton;
+
+import com.aether.present.state.LoginPresenter;
+import com.aether.present.state.LoginView;
+import com.jme.input.KeyInput;
+import com.jme.system.DisplaySystem;
+import com.jmex.bui.BButton;
+import com.jmex.bui.BLabel;
+import com.jmex.bui.BTextField;
+import com.jmex.bui.BPasswordField;
+import com.jmex.bui.BWindow;
+import com.jmex.bui.BuiSystem;
+import com.jmex.bui.event.ActionEvent;
+import com.jmex.bui.event.ActionListener;
+import com.jmex.bui.layout.AbsoluteLayout;
+import com.jmex.bui.util.Point;
+import com.jmex.game.state.GameStateManager;
+
+@Singleton
+public class LoginWindow extends BaseWindow implements LoginView {
+	private static final Object DEFAULT_WINDOW = "base.window";
+	private static final String EXIT_KEY_BINDING = "exit";
+        private static final String LOGIN_KEY_BINDING = "login";
+	private static final int MENU_ITEMS = 4;
+	private static final int BUTTON_HEIGHT = 20;
+	private static final int BUTTON_WIDTH = 50;
+
+        private BLabel welcome;
+        private BLabel userLabel;
+        private BLabel pswdLabel;
+
+        private BTextField username;
+        private BPasswordField password;
+
+	private BButton login;
+	private BButton exitGame;
+
+	private LoginPresenter presenter;
+	private int startPosY;
+
+    public LoginWindow()
+    {
+        super(ID);
+
+        bindKeysToView();
+
+        DisplaySystem.getDisplaySystem();
+        BWindow mainWindow = new BWindow(BuiSystem.getStyle(), new AbsoluteLayout());
+        mainWindow.setName(ID);
+        addWindow(DEFAULT_WINDOW, mainWindow);
+        mainWindow.setSize(BUTTON_WIDTH + 150, BUTTON_HEIGHT * MENU_ITEMS + 220);
+
+        startPosY = mainWindow.getHeight() - 100;
+        int posX = (mainWindow.getWidth() - BUTTON_WIDTH) / 2;
+
+        welcome = new BLabel("Welcome to AEther");
+        welcome.setPreferredSize(BUTTON_WIDTH + 80, BUTTON_HEIGHT);
+        mainWindow.add(welcome, new Point(posX /2, startPosY));
+
+        userLabel = new BLabel("username:");
+        userLabel.setPreferredSize(BUTTON_WIDTH , BUTTON_HEIGHT);
+        mainWindow.add(userLabel, new Point(posX /2,startPosY - 200));
+
+        username = new BTextField(30);
+        mainWindow.add(username,new Point(posX /2,startPosY - 225));
+
+        pswdLabel = new BLabel("password:");
+        pswdLabel.setPreferredSize(BUTTON_WIDTH , BUTTON_HEIGHT);
+        mainWindow.add(pswdLabel,new Point(posX /2,startPosY - 235));
+
+        password = new BPasswordField(30);
+        mainWindow.add(password, new Point(posX /2, startPosY - 260));
+
+        mainWindow.center();
+
+        initLoginButton(posX);
+        initExitGameButton(posX);
+
+        GameStateManager.getInstance().attachChild(this);
+    }
+
+	private void initLoginButton(int posX)
+        {
+            login = new BButton("login");
+            login.setPreferredSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+            getWindow(DEFAULT_WINDOW).add(login, new Point(posX /2 - BUTTON_WIDTH, startPosY - 290));
+            login.addListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent event) {
+                presenter.performLoginAttempt();
+			}
+		});
+	}
+
+	private void initExitGameButton(int posX)
+        {
+            exitGame = new BButton("Exit");
+            exitGame.setPreferredSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+            getWindow(DEFAULT_WINDOW).add(exitGame, new Point(posX /2 + 10, startPosY - 290));
+            exitGame.addListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent event) {
+                            presenter.performExit();
+                    }
+            });
+	}
+
+	private void bindKeysToView()
+        {
+            registerBinding(EXIT_KEY_BINDING, KeyInput.KEY_ESCAPE);
+            registerBinding(LOGIN_KEY_BINDING,KeyInput.KEY_RETURN);
+
+	}
+
+	@Override
+	protected void handleBinding(String name)
+        {
+            if (EXIT_KEY_BINDING.equals(name))
+            {
+                presenter.performExit();
+            }
+            else if(LOGIN_KEY_BINDING.equals(name))
+            {
+                presenter.performLoginAttempt();
+            }
+	}
+
+	@Override
+	public void setPresenter(LoginPresenter presenter)
+        {
+            this.presenter = presenter;
+	}
+}
